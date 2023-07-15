e81d64c00d340c50ffd47db10329084a532999e8
tutonroy1807 <prosenjitroy1807@gmail.com>
2023-07-16 02:23:44 +0600

    Work With matarial app class and include with theme, darktheme & debugmode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,35 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage(),);
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: HomePage(),
+      darkTheme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+    ); // we are work in material app
   }
-
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(title: Text("Driver Haiyer",)),
+      appBar: AppBar(
+          title: Text(
+        "Driver Haiyer",
+      )),
       body: Text("MY APP"),
     );
   }
-
 }
